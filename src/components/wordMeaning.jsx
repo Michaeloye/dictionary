4b66a96b14e143b37816698c70340f7754a5fe25
michaeloye <michaeloye23@gmail.com>
2022-01-14 23:32:37 +0100

    Add props and update design layout

--- src/components/wordMeaning.jsx
+++ src/components/wordMeaning.jsx
@@ -1,25 +1,32 @@
 import React from "react";
+import SynonymCard from "./synonymCard";
 
 function WordMeaning({ partOfSpeech, definitions }) {
   return (
-    <div className="mt-3">
-      <p className="text-white ">
+    <div className="text-lg">
+      <p className="text-white mt-5 ml-5">
         <i>{partOfSpeech}</i>
       </p>
       {Boolean(definitions.length) &&
         definitions.map((definition) => (
           <div>
-            <p className="text-text-blue">{definition.definition}</p>
-            <p className="text-text-blue">
+            <p className="text-text-blue mt-3">
+              <span className="text-white">meaning: </span>
+              {definition.definition}
+            </p>
+            <p className="text-text-blue mt-2">
               <span className="text-white">example: </span>
               {definition.example}
             </p>
             <div>
-              <p>
-                similar:{" "}
-                {Boolean(definition.synonyms.length) &&
-                  definition.synonyms.map((synonym) => synonym)}
-              </p>
+              {Boolean(definition.synonyms.length) && (
+                <div className="flex flex-wrap mt-2 text-white items-start">
+                  <p>similar: </p>
+                  {definition.synonyms.map((synonym) => (
+                    <SynonymCard word={synonym} />
+                  ))}
+                </div>
+              )}
             </div>
           </div>
         ))}
