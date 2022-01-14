e4a439cd4c610d09ec527924ad194ae128f3b09b
michaeloye <michaeloye23@gmail.com>
2022-01-14 18:38:40 +0100

    Use axios to fetch data

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,13 +1,31 @@
 import React, { useState, useEffect } from "react";
 import Search from "./search";
 import { GiSpeaker } from "react-icons/gi";
+import axios from "axios";
 
 function Main() {
+  const [word, setWord] = useState();
   const [keyword, setKeyword] = useState("Hello");
-  const [transcription, setTranscription] = useState("/həˈloʊ/");
+  const [transcription, setTranscription] = useState("həˈloʊ");
+
+  function submitForm(e) {
+    e.preventDefault();
+    axios
+      .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+      .then((res) => {
+        console.log(res.data[0]);
+        setKeyword(res.data[0].word);
+        setTranscription(res.data[0].phonetic);
+      })
+      .catch((error) => console.log(error));
+  }
+  function getInput(e) {
+    setWord(e.target.value);
+  }
+
   return (
     <div className="h-screen bg-body-blue">
-      <Search />
+      <Search submitForm={submitForm} getInput={getInput} />
       <div className="px-10 lg:px-20 mt-10">
         <div className="name-and-audio flex items-end">
           <p className="text-white font-medium text-4xl">{keyword}</p>
