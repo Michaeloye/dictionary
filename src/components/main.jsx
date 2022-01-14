9f72ff3ad24fb3b00676239cc522575029c9382e
michaeloye <michaeloye23@gmail.com>
2022-01-14 23:33:18 +0100

    Add WordMeaning component

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,20 +2,24 @@ import React, { useState, useEffect } from "react";
 import Search from "./search";
 import { GiSpeaker } from "react-icons/gi";
 import axios from "axios";
+import WordMeaning from "./wordMeaning";
 
 function Main() {
   const [word, setWord] = useState();
   const [keyword, setKeyword] = useState("Hello");
   const [transcription, setTranscription] = useState("həˈloʊ");
+  const [meanings, setMeanings] = useState([]);
 
   function submitForm(e) {
     e.preventDefault();
     axios
       .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
       .then((res) => {
-        console.log(res.data[0]);
-        setKeyword(res.data[0].word);
-        setTranscription(res.data[0].phonetic);
+        const data = res.data[0];
+        console.log(data);
+        setKeyword(data.word);
+        setTranscription(data.phonetic);
+        setMeanings(data.meanings);
       })
       .catch((error) => console.log(error));
   }
@@ -24,16 +28,26 @@ function Main() {
   }
 
   return (
-    <div className="h-screen bg-body-blue">
+    <div className=" bg-body-blue">
       <Search submitForm={submitForm} getInput={getInput} />
-      <div className="px-10 lg:px-20 mt-10">
+      <div className="px-7 lg:px-20 mt-10">
         <div className="name-and-audio flex items-end">
           <p className="text-white font-medium text-4xl">{keyword}</p>
           <div>
             <GiSpeaker className="text-text-blue" size={38} />
           </div>
         </div>
-        <p className="font-medium text-lg text-text-blue">{transcription}</p>
+        <p className="font-medium text-lg text-text-blue -mb-3">
+          {transcription}
+        </p>
+        {Boolean(meanings.length) &&
+          meanings.map((meaning) => (
+            <WordMeaning
+              key={meaning.partOfSpeech}
+              partOfSpeech={meaning.partOfSpeech}
+              definitions={meaning.definitions}
+            />
+          ))}
       </div>
     </div>
   );
