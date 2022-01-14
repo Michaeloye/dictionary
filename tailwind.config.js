1a36ca953dc5a8cf3921f85c3394ceb0d79954c1
michaeloye <michaeloye23@gmail.com>
2022-01-14 14:02:30 +0100

    Add color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "header-blue": "#192ABE",
+      },
+    },
   },
   plugins: [],
 };
