d206ce8ee1c3292d7e2135c9ae02e9379b7e723d
michaeloye <michaeloye23@gmail.com>
2022-01-16 17:13:22 +0100

    Add Footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="text-center text-sm mt-16 mb-3 text-text-blue">
+      made by{" "}
+      <a href="https://github.com/Michaeloye" className="text-white">
+        Oyebadejo Michael
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
