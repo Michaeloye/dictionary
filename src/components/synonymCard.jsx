b0762fd7104e84f5f0363c75ffb4ac39f324bb71
michaeloye <michaeloye23@gmail.com>
2022-01-16 16:59:01 +0100

    Add callback prop

--- src/components/synonymCard.jsx
+++ src/components/synonymCard.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-function SynonymCard({ word }) {
+function SynonymCard({ word, searchWordFromSynonym }) {
   return (
     <div
       className="inline-block rounded-full border border-black ml-3 mt-1 
       bg-synonym-blue text-text-blue px-3 py-1 text-center text-sm cursor-pointer"
+      onClick={() => searchWordFromSynonym(word)}
     >
       {word}
     </div>
