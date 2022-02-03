fa41c46f691cbba3a4dae32d945793806fde3cb4
michaeloye <michaeloye23@gmail.com>
2022-02-03 23:13:11 +0100

    Update margin top

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="text-center text-sm mt-16 mb-3 text-text-blue">
+    <footer className="text-center text-sm mt-14 mb-3 text-text-blue">
       made by{" "}
       <a href="https://github.com/Michaeloye" className="text-white">
         Oyebadejo Michael
